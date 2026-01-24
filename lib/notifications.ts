2df0b187ebd86fbe271c867f7a605b657eda832c
malokuva-debug <malokuva@gmail.com>
2026-01-24 17:45:45 +0100

    Update notifications.ts

--- lib/notifications.ts
+++ lib/notifications.ts
@@ -57,7 +57,7 @@ export const subscribeToPush = async (
 
     const subscription = await registration.pushManager.subscribe({
       userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
+      applicationServerKey: urlBase64ToUint8Array(vapidPublicKey) as BufferSource,
     });
 
     console.log('Push subscription created:', subscription);
