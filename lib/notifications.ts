1e13eff4c419edf3e98691488fe2b56edd6c0d26
malokuva-debug <malokuva@gmail.com>
2026-01-24 12:26:06 +0100

    Update notifications.ts

--- lib/notifications.ts
+++ lib/notifications.ts
@@ -1,10 +1,6 @@
-// /lib/notifications.ts
 import { sendNotification } from './magicbell.server';
 
-export async function notifyWorkerNewAppointment(
-  userId: string,
-  data: any
-) {
+export async function notifyWorkerNewAppointment(userId: string, data: any) {
   return sendNotification(
     userId,
     'New appointment',
