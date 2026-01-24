cf4ca9f56eade8f2c6106cdfdc8d8af136dabc9c
malokuva-debug <malokuva@gmail.com>
2026-01-24 12:06:09 +0100

    Create notifications.ts

---
+++ lib/notifications.ts
@@ -0,0 +1,13 @@
+import { sendNotification } from './magicbell.server';
+
+export async function notifyWorkerNewAppointment(
+  userId: string,
+  data: any
+) {
+  return sendNotification(
+    userId,
+    'New appointment',
+    `Client: ${data.clientName}`,
+    data.url
+  );
+}
