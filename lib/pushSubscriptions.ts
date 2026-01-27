29177fa1293e8060abd7926ef660dd5f703496fc
malokuva-debug <malokuva@gmail.com>
2026-01-27 17:50:07 +0100

    Delete pushSubscriptions.ts

--- lib/pushSubscriptions.ts
+++
@@ -1,8 +0,0 @@
-// lib/pushSubscriptions.ts
-export const g = global as typeof globalThis & {
-  pushSubscriptions: Map<string, any>;
-};
-
-if (!g.pushSubscriptions) {
-  g.pushSubscriptions = new Map<string, any>();
-}
